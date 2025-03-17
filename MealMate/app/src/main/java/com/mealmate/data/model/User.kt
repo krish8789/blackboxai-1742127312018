b7d426d9756d664a4a6041510790bce39443db65
krish8789 <126272939+krish8789@users.noreply.github.com>
2025-03-17 07:40:27 +0545

    Add MealMate/app/src/main/java/com/mealmate/data/model/User.kt 1742176527032

---
+++ MealMate/app/src/main/java/com/mealmate/data/model/User.kt
@@ -0,0 +1,13 @@
+package com.mealmate.data.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "users")
+data class User(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    val name: String,
+    val email: String,
+    val password: String // In a real app, this should be properly hashed
+)
